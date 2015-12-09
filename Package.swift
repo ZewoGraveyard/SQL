210d59b67e2c210fd90401688d4e03b341d67d40
David Ask <david@formbound.com>
2015-12-09 18:19:21 +0100

    Updated package name

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftSQL",
+    name: "SQL",
     dependencies: [
         .Package(url: "https://github.com/Zewo/URI.git", majorVersion: 0)
     ]
