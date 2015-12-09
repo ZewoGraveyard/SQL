df4d244c9e7b542d3adc379cfabef124c331ed3a
David Ask <david@formbound.com>
2015-12-09 18:24:39 +0100

    Specifying minor version for URI

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "SQL",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/URI.git", majorVersion: 0)
+        .Package(url: "https://github.com/Zewo/URI.git", majorVersion: 0, minor: 1)
     ]
 )
