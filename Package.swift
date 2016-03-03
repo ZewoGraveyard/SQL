9e9bcdbffad2faf74ab39534ea0c18480dbf6016
David Ask <david@formbound.com>
2016-03-03 12:04:33 +0100

    Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,6 @@ let package = Package(
     dependencies: [
     	.Package(url: "https://github.com/Zewo/Data.git", majorVersion: 0, minor: 2),
         .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/Zewo/Clibvenice.git", majorVersion: 0, minor: 2),
         .Package(url: "https://github.com/Zewo/Log.git", majorVersion: 0, minor: 2),
         .Package(url: "https://github.com/Zewo/File.git", majorVersion: 0, minor: 2)
     ]
