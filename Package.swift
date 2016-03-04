b3991e1adf88385f4cf88061e2c53bc84dd28c02
Paulo Faria <paulo.faria.rl@gmail.com>
2016-03-04 17:21:10 -0300

    add CLibvenice

--- Package.swift
+++ Package.swift
@@ -6,6 +6,7 @@ let package = Package(
     	.Package(url: "https://github.com/Zewo/Data.git", majorVersion: 0, minor: 2),
         .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 2),
         .Package(url: "https://github.com/Zewo/Log.git", majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/Zewo/File.git", majorVersion: 0, minor: 2)
+        .Package(url: "https://github.com/Zewo/File.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/Zewo/CLibvenice.git", majorVersion: 0, minor: 2)
     ]
 )
