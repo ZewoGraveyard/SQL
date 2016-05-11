b258c0858e9660831720f406897ec8b5786ca21a
John Coates <acapulco1988@gmail.com>
2016-05-10 20:04:48 -0700

    Updated for Swift snapshot 05-03 rev2
    Use previous format for version numbers. Didn't realize it was the same.

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
     name: "SQL",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/String.git", versions: Version(0,7,0)..<Version(0,8,0)),
-        .Package(url: "https://github.com/Zewo/URI.git", versions: Version(0,7,0)..<Version(0,8,0)),
-        .Package(url: "https://github.com/Zewo/Log.git", versions: Version(0,6,0)..<Version(0,7,0))
+        .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/Zewo/URI.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/Zewo/Log.git", majorVersion: 0, minor: 6)
     ]
 )
