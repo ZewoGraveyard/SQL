82206d0d3bfe69b5702f17169d889faea27cfeda
Jörn Röder <kontakt@joernroeder.de>
2016-09-27 22:25:44 +0200

    Zewo/Core as only dependency

--- Package.swift
+++ Package.swift
@@ -3,7 +3,6 @@ import PackageDescription
 let package = Package(
     name: "SQL",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 7),
-        .Package(url: "https://github.com/Zewo/URI.git", majorVersion: 0, minor: 8),
+        .Package(url: "https://github.com/Zewo/Core.git", majorVersion: 0, minor: 13)
     ]
 )
