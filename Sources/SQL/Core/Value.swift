cecd4b1150e0bc546722488499ddd5709b2343d5
Dan Appel <dan.appel00@gmail.com>
2016-11-01 16:56:02 -0700

    Conform more types to ValueConvertible

--- Sources/SQL/Core/Value.swift
+++ Sources/SQL/Core/Value.swift
@@ -1,4 +1,5 @@
 import Axis
+import Foundation
 
 public enum Value {
     case string(String)
@@ -21,30 +22,33 @@ extension ValueConvertible {
     }
 }
 
-
-extension Int: ValueConvertible {
+extension Buffer: ValueConvertible {
     public init(rawSQLData buffer: Buffer) throws {
-        guard let value = Int(try String(buffer: buffer)) else {
-            throw ValueError(description: "Failed to convert data to Int")
-        }
-        self = value
+        self = buffer
     }
 
     public var sqlValue: Value {
-        return .string(String(self))
+        return .buffer(self)
     }
 }
 
-extension UInt: ValueConvertible {
+extension String: ValueConvertible {
     public init(rawSQLData buffer: Buffer) throws {
-        guard let value = UInt(try String(buffer: buffer)) else {
-            throw ValueError(description: "Failed to convert data to UInt")
-        }
-        self = value
+        try self.init(buffer: buffer)
     }
 
     public var sqlValue: Value {
-        return .string(String(self))
+        return .string(self)
+    }
+}
+
+extension Data : ValueConvertible {
+    public init(rawSQLData buffer: Buffer) throws {
+        self.init(buffer.bytes)
+    }
+
+    public var sqlValue: Value {
+        return .buffer(Buffer(Array(self)))
     }
 }
 
@@ -74,22 +78,132 @@ extension Double: ValueConvertible {
     }
 }
 
-extension String: ValueConvertible {
+extension Int : ValueConvertible {
     public init(rawSQLData buffer: Buffer) throws {
-        try self.init(buffer: buffer)
+        guard let value = Int(try String(buffer: buffer)) else {
+            throw ValueError(description: "Failed to convert data to Int")
+        }
+        self = value
     }
 
     public var sqlValue: Value {
-        return .string(self)
+        return .string(String(self))
     }
 }
 
-extension Buffer: ValueConvertible {
+extension Int8 : ValueConvertible {
     public init(rawSQLData buffer: Buffer) throws {
-        self = buffer
+        guard let value = Int8(try String(buffer: buffer)) else {
+            throw ValueError(description: "Failed to convert data to Int")
+        }
+        self = value
     }
 
     public var sqlValue: Value {
-        return .buffer(self)
+        return .string(String(self))
+    }
+}
+
+extension Int16 : ValueConvertible {
+    public init(rawSQLData buffer: Buffer) throws {
+        guard let value = Int16(try String(buffer: buffer)) else {
+            throw ValueError(description: "Failed to convert data to Int")
+        }
+        self = value
+    }
+
+    public var sqlValue: Value {
+        return .string(String(self))
+    }
+}
+
+extension Int32 : ValueConvertible {
+    public init(rawSQLData buffer: Buffer) throws {
+        guard let value = Int32(try String(buffer: buffer)) else {
+            throw ValueError(description: "Failed to convert data to Int")
+        }
+        self = value
+    }
+
+    public var sqlValue: Value {
+        return .string(String(self))
+    }
+}
+
+extension Int64 : ValueConvertible {
+    public init(rawSQLData buffer: Buffer) throws {
+        guard let value = Int64(try String(buffer: buffer)) else {
+            throw ValueError(description: "Failed to convert data to Int")
+        }
+        self = value
+    }
+
+    public var sqlValue: Value {
+        return .string(String(self))
+    }
+}
+
+extension UInt: ValueConvertible {
+    public init(rawSQLData buffer: Buffer) throws {
+        guard let value = UInt(try String(buffer: buffer)) else {
+            throw ValueError(description: "Failed to convert data to UInt")
+        }
+        self = value
+    }
+
+    public var sqlValue: Value {
+        return .string(String(self))
+    }
+}
+
+extension UInt8: ValueConvertible {
+    public init(rawSQLData buffer: Buffer) throws {
+        guard let value = UInt8(try String(buffer: buffer)) else {
+            throw ValueError(description: "Failed to convert data to UInt")
+        }
+        self = value
+    }
+
+    public var sqlValue: Value {
+        return .string(String(self))
+    }
+}
+
+extension UInt16: ValueConvertible {
+    public init(rawSQLData buffer: Buffer) throws {
+        guard let value = UInt16(try String(buffer: buffer)) else {
+            throw ValueError(description: "Failed to convert data to UInt")
+        }
+        self = value
+    }
+
+    public var sqlValue: Value {
+        return .string(String(self))
+    }
+}
+
+extension UInt32: ValueConvertible {
+    public init(rawSQLData buffer: Buffer) throws {
+        guard let value = UInt32(try String(buffer: buffer)) else {
+            throw ValueError(description: "Failed to convert data to UInt")
+        }
+        self = value
+    }
+
+    public var sqlValue: Value {
+        return .string(String(self))
+    }
+}
+
+extension UInt64: ValueConvertible {
+    public init(rawSQLData buffer: Buffer) throws {
+        guard let value = UInt64(try String(buffer: buffer)) else {
+            throw ValueError(description: "Failed to convert data to UInt")
+        }
+        self = value
+    }
+
+    public var sqlValue: Value {
+        return .string(String(self))
     }
 }
